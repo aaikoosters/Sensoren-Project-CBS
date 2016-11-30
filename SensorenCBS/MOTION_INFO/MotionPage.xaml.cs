ab8442ae8e94c55da0f14e83d463ef216f01d5b9
AaikOosters <aaik.oosters@gmail.com>
2016-11-30 17:01:02 +0100

    MotionDetection
    ToDo: Uitwerken hoe de functies en de plugin werken
    Done in MotionPage:
    - Gemonitord of de device opgepakt word of niet
    - Gemonitord de x, y en z richtingen.
    
    Variable gegevens die gebruikt zijn moeten nog nader gespecificeerd
    worden hoe deze werken

--- SensorenCBS/MOTION_INFO/MotionPage.xaml.cs
+++ SensorenCBS/MOTION_INFO/MotionPage.xaml.cs
@@ -10,14 +10,22 @@ namespace SensorenCBS
 	public partial class MotionPage : ContentPage
 	{
 		public const MotionSensorDelay Default = (MotionSensorDelay)1000;
+		private float[] mGravity;
+		private float mAccel;
+		private float mAccelCurrent;
+		private float mAccelLast;
+		private int opgetild = 0;
+
 
 		public MotionPage()
 		{
 			InitializeComponent();
 
-			var slow = (MotionSensorDelay)01000000000;
-			var counter = 10;
-			var getal = 1;
+
+			mAccelLast = 9.80665f;
+			mAccelCurrent = 9.80665f;
+			mAccel = 0.00f;
+
 
 			//CrossDeviceMotion.Current.Start(MotionSensorDelay.Default);
 
@@ -29,14 +37,37 @@ namespace SensorenCBS
 				{
 					
 					case MotionSensorType.Accelerometer:
-						Debug.WriteLineIf(slow.Equals(counter), " slow is geweest on update");
-						getal++;
-						Debug.WriteLine(slow + ",A " + getal);
+
 						//Debug.WriteLine("A: {0},{1},{2}", ((MotionVector)a.Value).X, ((MotionVector)a.Value).Y, ((MotionVector)a.Value).Z);
 
-						lblX.Text = "X: " + ((MotionVector)a.Value).X;
-						lblY.Text = "Y: " + ((MotionVector)a.Value).Y;
-						lblZ.Text = "Z: " + ((MotionVector)a.Value).Z;
+						//lblX.Text = "X: " + ((MotionVector)a.Value).X;
+						//lblY.Text = "Y: " + ((MotionVector)a.Value).Y;
+						//lblZ.Text = "Z: " + ((MotionVector)a.Value).Z;
+						float xx = (float)((MotionVector)a.Value).X;
+						float yy = (float)((MotionVector)a.Value).Y;
+						float zz = (float)((MotionVector)a.Value).Z;
+
+
+						mAccelLast = mAccelCurrent;
+						mAccelCurrent = (float)Math.Sqrt(xx * xx + yy * yy + zz * zz);
+						float delta = mAccelCurrent - mAccelLast;
+						Debug.WriteLine("mAccel = {0}", mAccel); // mAccel is gelijk aan x tot dat er een keer bewogen is
+
+						mAccel = mAccel * 1.0f + delta;
+						if (mAccel > 2)
+						{
+							opgetild++;
+							lblOp.Text = opgetild.ToString();
+							Debug.WriteLine("mAccel = {0} * {1} + {2} ", mAccel, 0.7f, delta);
+							Debug.WriteLine("mAccel: {0},Delta: {1},mAccelLast: {2}, mAccelCurrent {3}", mAccel.ToString(), delta.ToString(), mAccelLast.ToString(), mAccelCurrent.ToString());
+							//lblAccel.Text = ("mAccel: {0}, delta: {1}, mAccelLast {2}, mAccelCurrent {3}", mAccel.ToString(), delta.ToString(), mAccelLast.ToString(), mAccelCurrent.ToString());
+							lblAccel.Text = "Something happend: " + DateTime.Now;
+						}
+
+						lblZ.Text = zz.ToString();
+						lblX.Text = xx.ToString();
+						lblY.Text = yy.ToString();
+
 						break;
 
 				}
