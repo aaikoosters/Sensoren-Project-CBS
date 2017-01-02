eb33efaaff79f20b99a7246a049e9022fd436471
AaikOosters <aaik.oosters@gmail.com>
2017-01-02 13:45:19 +0100

    background start not working

---
+++ Droid/AutoStartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace SensorenCBS.Droid
+{
+	public class AutoStartUp : Service
+	{
+		public override void OnCreate()
+		{
+			base.OnCreate();
+			Toast.MakeText(this, "Service Started", ToastLength.Long).Show();
+		}
+		public override IBinder OnBind(Intent intent)
+		{
+			return null;
+		}
+	}
+}
---
+++ Droid/BootCompletedBroadcastReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+
+namespace SensorenCBS.Droid
+{
+	//[BroadcastReceiver]
+	//[IntentFilter(new[] { Intent.ActionBootCompleted })]
+	public class BootCompletedBroadcastReceiver : BroadcastReceiver
+	{
+		public BootCompletedBroadcastReceiver()
+		{
+		}
+
+		public override void OnReceive(Context context, Intent intent)
+		{
+			if (intent.Action == Intent.ActionBootCompleted)
+			{
+				Toast.MakeText(context, "Broadcast catched, start own application", ToastLength.Long).Show();
+				Intent applicationIntent = new Intent(context, typeof(MainActivity));
+				applicationIntent.AddFlags(ActivityFlags.NewTask);
+				context.StartActivity(applicationIntent);
+			}
+		}
+	}
+}
