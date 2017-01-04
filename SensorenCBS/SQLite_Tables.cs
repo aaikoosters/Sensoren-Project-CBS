77b871ca3fde7fdacbd1596d3483506295111342
AaikOosters <aaik.oosters@gmail.com>
2017-01-04 13:45:59 +0100

    tussentijdse commit

---
+++ SensorenCBS/SQLite_Tables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SensorenCBS
+{
+	public class SQLite_Tables
+	{
+		public class PickedUpTable
+		{
+			[PrimaryKey, AutoIncrement]
+			public int ID { get; set; }
+			public DateTime timeAndDay { get; set; }
+			public bool pickedUp { get; set; }
+		}
+	}
+}
