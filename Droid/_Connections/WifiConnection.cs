a37158421b022b1a830bcdd661ff4a7dd6048ddd
AaikOosters <aaik.oosters@gmail.com>
2017-01-10 15:11:55 +0100

    comment code

--- Droid/_Connections/WifiConnection.cs
+++ Droid/_Connections/WifiConnection.cs
@@ -12,9 +12,15 @@ namespace SensorenCBS.Droid
 {
 	public class WifiConnection : IWifiConnection
 	{
+		/*
+		 * The WiFiManager and the WiFiInfo are the Assembly Implementation to use/read information from WiFi
+		 * U need to use Android.Net.Wifi to implement it.
+		 * 
+		 * */
 		WifiManager wifiManager;
 		WifiInfo wifiInfo;
 
+		// inheritance from IWifiConnection
 		public string WifiSSID { get; set; }
 		public string WifiBSSID { get; set; }
 		public int WifiFrequency { get; set; }
@@ -25,10 +31,11 @@ namespace SensorenCBS.Droid
 		public int WifiRssi { get; set; }
 		public string WifiRssiLevel { get; set; }
 		public List<string> AllWifiBssids { get; set; }
-
+		// private list to add bssids to the public stack: AllWifiBssids
 		List<string> _wifiBssids = new List<string>();
 
 
+		// With this u can check the SSID from the connected WiFi
 		public void CheckWifiSSID()
 		{
 			wifiManager = (WifiManager)Application.Context.GetSystemService(Context.WifiService);
@@ -36,12 +43,12 @@ namespace SensorenCBS.Droid
 			WifiSSID = wifiInfo.SSID + ", " + wifiInfo.HiddenSSID;
 		}
 
+		// the BSSID, in the form of a six-byte MAC address: XX:XX:XX:XX:XX:XX
 		public void CheckWifiBBSID()
 		{
-			
 			wifiManager = (WifiManager)Application.Context.GetSystemService(Context.WifiService);
 			wifiInfo = wifiManager.ConnectionInfo;
-
+			// BSSID is the name of the acces point u are using
 			WifiBSSID = wifiInfo.BSSID;
 		}
 
@@ -54,7 +61,9 @@ namespace SensorenCBS.Droid
 			WifiIpAddress = wifiInfo.IpAddress.ToString();
 			WifiLinkSpeed = wifiInfo.LinkSpeed;
 			WifiMacAddress = wifiInfo.MacAddress;
+			// the network ID, or -1 if there is no currently connected network 
 			WifiNetworkId = wifiInfo.NetworkId;
+			// Returns the received signal strength indicator of the current 802.11 network, in dBm. 
 			WifiRssi = wifiInfo.Rssi;
 		}
 
@@ -62,7 +71,7 @@ namespace SensorenCBS.Droid
 		{
 			wifiManager = (WifiManager)Application.Context.GetSystemService(Context.WifiService);
 			wifiInfo = wifiManager.ConnectionInfo;
-
+			// In this method there is a check or the bssid is a new/unique bssid
 			if (_wifiBssids.Count == 0 || _wifiBssids[(_wifiBssids.Count - 1)] != wifiInfo.BSSID)
 			{
 				_wifiBssids.Add(wifiInfo.BSSID);
